613acbe82a017c1bd068b0f4a8ce5851bbaebd1b
cmpbj <magnocarlos3232@gmail.com>
2022-03-12 16:24:20 +0000

    arquivo gasolina.py gera um gráfico de linha mostrando a variação de preço da gasolina nos 10 primeiros dias de julho de 2021

---
+++ gasolina.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import seaborn as sns
+
+df_gasolina_sp = pd.read_csv('gasolina.csv', sep=',')
+
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(data=df_gasolina_sp, x='dia', y='venda', palette='pastel')
+  grafico.set(title='Preço médio gasolina em SP nos 10 primeiros dias de Julho (2021)',
+              xlabel='Dia', ylabel='Preço');
+  plt.savefig(fname='gasolinaSP.png', box_inches='tight')
