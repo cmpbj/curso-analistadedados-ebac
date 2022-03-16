31a802cd3232336ffd505a6b42b4ebda5eeb9b24
cmpbj <magnocarlos3232@gmail.com>
2022-03-16 13:01:42 +0000

    atualização do título do gráfico

--- gasolina.py
+++ gasolina.py
@@ -7,6 +7,6 @@ df_gasolina_sp = pd.read_csv('gasolina.csv', sep=',')
 
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data=df_gasolina_sp, x='dia', y='venda', palette='pastel')
-  grafico.set(title='Preço médio gasolina em SP nos 10 primeiros dias de Julho (2021)',
+  grafico.set(title='Preço médio gasolina em SP nos 10 primeiros dias de Julho de 2021',
               xlabel='Dia', ylabel='Preço');
   plt.savefig(fname='gasolinaSP.png', box_inches='tight')
